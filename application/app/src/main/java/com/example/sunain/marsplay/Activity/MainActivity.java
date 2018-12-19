ed51fa126feb06576206886876477a4ccb591894
Sunain <sunain99@gmail.com>
2018-12-20 02:00:04 +0530

    1st update

--- application/app/src/main/java/com/example/sunain/marsplay/Activity/MainActivity.java
+++ application/app/src/main/java/com/example/sunain/marsplay/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.sunain.marsplay;
+package com.example.sunain.marsplay.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.sunain.marsplay.R;
 
 public class MainActivity extends AppCompatActivity {
 
